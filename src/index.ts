f0abf99ac390152a60dbddce75b03c938a213a2f
syuilo <syuilotan@yahoo.co.jp>
2020-04-11 22:12:00 +0900

    a

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './interpreter/index';
+export * as utils from './interpreter/util';
+export * as values from './interpreter/value';
+export const parse = require('./parser/parser.js').parse;
