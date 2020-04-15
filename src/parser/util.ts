fc6d70e0d8f6ca10be63409c66dc60d1af56dbba
Marihachi <marihachi0620@gmail.com>
2020-04-15 23:44:25 +0900

    Support template literal (#31)
    * support template literal
    
    * :cat:

---
+++ src/parser/util.ts
@@ -0,0 +1,43 @@
+export function createNode(type: string, params: Record<string, any>, children?: any[]) {
+	const node = { type };
+	params.children = children;
+	for (const key of Object.keys(params)) {
+		if (params[key] != null) {
+			node[key] = params[key];
+		}
+	}
+	return node;
+}
+
+export function group<T>(arr: T[], predicate: (prev: T, current: T) => boolean): T[][] {
+	const dest: any[][] = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		if (i != 0 && predicate(arr[i - 1], arr[i])) {
+			dest[dest.length - 1].push(arr[i]);
+		}
+		else {
+			dest.push([arr[i]]);
+		}
+	}
+
+	return dest;
+}
+
+export function ungroup<T>(groupes: T[][]): T[] {
+	return groupes.reduce((acc, val) => acc.concat(val), []);
+}
+
+export function concatTemplate(arr: any[]) {
+	let groupes = group(arr, (prev, current) => (typeof current == typeof prev));
+
+	// concat string
+	groupes = groupes.map(g => {
+		if (typeof g[0] == 'string') {
+			return [g.join('')];
+		}
+		return g;
+	});
+
+	return ungroup(groupes);
+}
