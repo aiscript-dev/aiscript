af6d12beaa652c3239ac359091ba3c75b890f42b
marihachi <marihachi0620@gmail.com>
2021-07-30 00:29:06 +0900

    [enhance] 予約語の判定を意味解析で行うように (#115)
    * keyword validation
    
    * validate some inner statements
    
    Co-authored-by: syuilo <Syuilotan@yahoo.co.jp>

--- src/analyzer/index.ts
+++ src/analyzer/index.ts
@@ -1,9 +1,15 @@
 import { Node } from '../node';
 import { RawAST } from '../parser';
 import { setAttribute } from './set-attribute';
+import { validateKeyword } from './validate-keyword';
 
 export function analyze(ast: RawAST): Node[] {
-	const nodes = setAttribute(ast.nodes);
+
+	// validation
+	validateKeyword(ast.nodes);
+
+	// transform
+	let nodes = setAttribute(ast.nodes);
 
 	return nodes;
 }
