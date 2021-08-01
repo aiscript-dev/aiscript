20f5a69a273cb5bdf48cf9b7c0165ca759df0313
syuilo <Syuilotan@yahoo.co.jp>
2021-08-01 20:06:17 +0900

    なんかいい感じなった
    Resolve #127

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,7 +3,7 @@
 * https://jestjs.io/docs/en/configuration.html
 */
 
-export default {
+module.exports = {
 	// All imported modules in your tests should be mocked automatically
 	// automock: false,
 
@@ -60,7 +60,11 @@ export default {
 	// globalTeardown: undefined,
 
 	// A set of global variables that need to be available in all test environments
-	// globals: {},
+	globals: {
+		"ts-jest": {
+			"useESM": true
+		}
+	},
 
 	// The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
 	// maxWorkers: "50%",
@@ -93,7 +97,7 @@ export default {
 	// notifyMode: "failure-change",
 
 	// A preset that is used as a base for Jest's configuration
-	// preset: undefined,
+	preset: "ts-jest/presets/js-with-ts-esm",
 
 	// Run tests from one or more projects
 	// projects: undefined,
