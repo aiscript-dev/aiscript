e4367bafb929e2fcdfba48e6013e92e98c3a3b21
marihachi <marihachi0620@gmail.com>
2021-12-05 21:40:42 +0900

    refactor (#159)
    * refactor
    
    * fix lint

--- src/interpreter/infix-to-fncall.ts
+++ src/interpreter/infix-to-fncall.ts
@@ -1,5 +1,5 @@
 import { AiScriptError } from './error';
-import { Node, NInfix, NCall, Loc } from '../node';
+import { Node, NInfix, NCall } from '../node';
 
 type OperatorInfo = {
 	func: string; // 対応する関数
@@ -14,34 +14,15 @@ type OperatorInfo = {
  * (1) (3)
  * ```
  */
-type InfixTree = Node | {
+type InfixTree = {
 	type: 'infixtree';
 	opInfo: OperatorInfo;
-	lchild: InfixTree;
-	rchild: InfixTree;
+	left: InfixTree | Node;
+	right: InfixTree | Node;
 };
 
-function makeOperatorInfo(operator: string): OperatorInfo {
-	switch (operator) {
-		case '+': return { func: 'Core:add', prec: 6 };
-		case '-': return { func: 'Core:sub', prec: 6 };
-		case '*': return { func: 'Core:mul', prec: 7 };
-		case '/': return { func: 'Core:div', prec: 7 };
-		case '%': return { func: 'Core:mod', prec: 7 };
-		case '==': return { func: 'Core:eq', prec: 4 };
-		case '!=': return { func: 'Core:neq', prec: 4 };
-		case '&&': return { func: 'Core:and', prec: 3 };
-		case '||': return { func: 'Core:or', prec: 2 };
-		case '<': return { func: 'Core:lt', prec: 4 };
-		case '>': return { func: 'Core:gt', prec: 4 };
-		case '<=': return { func: 'Core:lteq', prec: 4 };
-		case '>=': return { func: 'Core:gteq', prec: 4 };
-		default: throw new AiScriptError(`No such operator: ${operator}.`);
-	}
-}
-
-function makeInfixTree(opInfo: OperatorInfo, lchild: InfixTree, rchild: InfixTree): InfixTree {
-	return { type: 'infixtree', opInfo, lchild, rchild };
+function INFIX_TREE(opInfo: OperatorInfo, left: InfixTree | Node, right: InfixTree | Node): InfixTree {
+	return { type: 'infixtree', opInfo, left, right };
 }
 
 /**
@@ -67,44 +48,63 @@ function makeInfixTree(opInfo: OperatorInfo, lchild: InfixTree, rchild: InfixTre
  * - NOTE: 右結合性の演算子としては代入演算子などが挙げられる
  * - NOTE: 比較の演算子などは非結合性とされる
  */
-function insert(tree: InfixTree, nextOpInfo: OperatorInfo, nextTree: InfixTree): InfixTree {
+function insert(tree: InfixTree | Node, nextOpInfo: OperatorInfo, nextTree: InfixTree | Node): InfixTree {
 	if (tree.type !== 'infixtree') {
-		return makeInfixTree(nextOpInfo, tree, nextTree);
-	} else if (tree.opInfo.prec >= nextOpInfo.prec) {
-		return makeInfixTree(nextOpInfo, tree, nextTree);
-	} else {
-		const { opInfo, lchild, rchild } = tree;
-		return makeInfixTree(opInfo, lchild, insert(rchild, nextOpInfo, nextTree));
+		return INFIX_TREE(nextOpInfo, tree, nextTree);
 	}
+	if (tree.opInfo.prec >= nextOpInfo.prec) {
+		return INFIX_TREE(nextOpInfo, tree, nextTree);
+	}
+	const { opInfo, left, right } = tree;
+	return INFIX_TREE(opInfo, left, insert(right, nextOpInfo, nextTree));
+}
+
+const opInfoTable = {
+	'+': { func: 'Core:add', prec: 6 },
+	'-': { func: 'Core:sub', prec: 6 },
+	'*': { func: 'Core:mul', prec: 7 },
+	'/': { func: 'Core:div', prec: 7 },
+	'%': { func: 'Core:mod', prec: 7 },
+	'==': { func: 'Core:eq', prec: 4 },
+	'!=': { func: 'Core:neq', prec: 4 },
+	'&&': { func: 'Core:and', prec: 3 },
+	'||': { func: 'Core:or', prec: 2 },
+	'<': { func: 'Core:lt', prec: 4 },
+	'>': { func: 'Core:gt', prec: 4 },
+	'<=': { func: 'Core:lteq', prec: 4 },
+	'>=': { func: 'Core:gteq', prec: 4 },
+};
+
+function getOpInfo(operator: string): OperatorInfo {
+	const op = opInfoTable[operator];
+	if (op == null) {
+		throw new AiScriptError(`No such operator: ${operator}.`);
+	}
+	return op;
 }
 
 /**
  * 中置演算子式を表す木を対応する関数呼び出しの構造体に変換する
  */
-function ascall(tree: InfixTree): Node {
+function asCall(tree: InfixTree | Node): Node {
 	if (tree.type !== 'infixtree') {
 		return tree;
-	} else {
-		return {
-			type: 'call',
-			name: tree.opInfo.func,
-			args: [ ascall(tree.lchild), ascall(tree.rchild) ],
-		} as NCall;
 	}
+	return {
+		type: 'call',
+		name: tree.opInfo.func,
+		args: [ asCall(tree.left), asCall(tree.right) ],
+	} as NCall;
 }
 
 /**
  * NInfix を関数呼び出し形式に変換する
  */
 export function infixToFnCall(node: NInfix): Node {
-	const infos = node.operators.map(makeOperatorInfo);
-	const nodes = node.operands;
-
-	let tree = makeInfixTree(infos[0], nodes[0], nodes[1]);
-
-	for (let i = 1; i < infos.length; i++) {
-		tree = insert(tree, infos[i], nodes[i + 1]);
+	const opInfos = node.operators.map(op => getOpInfo(op));
+	let tree = INFIX_TREE(opInfos[0], node.operands[0], node.operands[1]);
+	for (let i = 1; i < opInfos.length; i++) {
+		tree = insert(tree, opInfos[i], node.operands[i + 1]);
 	}
-
-	return ascall(tree);
+	return asCall(tree);
 }
