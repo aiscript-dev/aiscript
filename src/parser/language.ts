f314910d8d86bd977bec067f61629de517e29a69
marihachi <marihachi0620@gmail.com>
2022-08-01 18:10:04 +0900

    preprocessor, string, template

--- src/parser/language.ts
+++ src/parser/language.ts
@@ -1,11 +1,153 @@
 import * as T from 'terrario';
 
+function group<T>(arr: T[], predicate: (prev: T, curr: T) => boolean): T[][] {
+	const dest: T[][] = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (i != 0 && predicate(arr[i - 1], arr[i])) {
+			dest[dest.length - 1].push(arr[i]);
+		} else {
+			dest.push([arr[i]]);
+		}
+	}
+	return dest;
+}
+
+function ungroup<T>(groupes: T[][]): T[] {
+	return groupes.reduce((acc, val) => acc.concat(val), []);
+}
+
+function createNode(type: string, params: Record<string, any>) {
+	const node: Record<string, any> = { type };
+	for (const key of Object.keys(params)) {
+		if (params[key] !== undefined) {
+			node[key] = params[key];
+		}
+	}
+	//const loc = location();
+	//node.loc = { start: loc.start.offset, end: loc.end.offset - 1 };
+	return node;
+}
+
+const eof = new T.Parser((input, index, state) => {
+	return index >= input.length
+		? T.success(index, null)
+		: T.failure();
+});
+
+function succeeded<T>(value: T): T.Parser<T> {
+	return new T.Parser((input, index, state) => {
+		return T.success(index, value);
+	});
+}
+
+const endOfLine = T.alt([T.newline, eof]);
+
 const language = T.createLanguage({
+	preprocess: r => {
+		const commentLine = T.seq([
+			T.str('//'),
+			T.seq([
+				T.notMatch(endOfLine),
+				T.char,
+			]).many(0),
+		]);
+
+		return T.alt([
+			r.string.text(),
+			r.template.text(),
+			commentLine.map(() => ''),
+			T.char,
+		]).many(0).map(values => {
+			return values.join('');
+		});
+	},
+
+	root: r => {
+		const statements = T.sep(r.statement, T.newline, 1);
+		return T.alt([
+			statements,
+			succeeded([]),
+		]);
+	},
+
 	statement: r => T.alt([
-		
+
 	]),
 
 	expression: r => T.alt([
-		
+
 	]),
+
+	string: r => {
+		const content = T.alt([
+			T.str('\\"').map(() => '"'),
+			T.char,
+		]);
+
+		return T.seq([
+			T.str('"'),
+			T.seq([
+				T.notMatch(T.str('"')),
+				content,
+			]).many(0).text(),
+			T.str('"'),
+		], 1).map(value => {
+			return createNode('str', { value: value });
+		});
+	},
+
+	template: r => {
+		function concatTemplate(arr: any[]): any[] {
+			let groupes = group(arr, (prev, current) => (typeof current == typeof prev));
+			// concat string
+			groupes = groupes.map(g => {
+				if (typeof g[0] == 'string') {
+					return [g.join('')];
+				}
+				return g;
+			});
+			return ungroup(groupes);
+		}
+
+		const embeded = T.seq([
+			T.str('{'),
+			r.expression,
+			T.str('}'),
+		], 1);
+
+		const content = T.alt([
+			T.str('\\{').map(() => '{'),
+			T.str('\\}').map(() => '}'),
+			T.str('\\`').map(() => '`'),
+			embeded,
+			T.char,
+		]);
+
+		return T.seq([
+			T.str('`'),
+			T.seq([
+				T.notMatch(T.str('`')),
+				content,
+			]).many(0),
+			T.str('`'),
+		], 1).map(value => {
+			return createNode('tmpl', { tmpl: concatTemplate(value) });
+		});
+	},
 });
+
+export function parse(input: string) {
+	let result;
+
+	result = language.preprocess.parse(input);
+	if (!result.success) {
+		throw new Error('syntax error: preprocess fails');
+	}
+
+	result = language.root.parse(result.value);
+	if (!result.success) {
+		throw new Error('syntax error');
+	}
+
+	return result.value;
+}
