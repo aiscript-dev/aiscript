8bc83455c616360a8d2713c58bb62e59d62a3bd1
marihachi <marihachi0620@gmail.com>
2022-08-20 13:57:19 +0900

    IR node

--- src/parser/ir-node.ts
+++ src/parser/ir-node.ts
@@ -7,14 +7,12 @@
 import { TypeSource } from '../type';
 
 export type Node = GlobalMember | StaticLiteral;
-export type GlobalMember = Namespace | Meta | LocalMember;
+export type GlobalMember = Namespace | Meta | Statement | Expression;
 export type NamespaceMember = Definition | Namespace;
-export type LocalMember = Statement | Expression;
 
 export type Statement =
 	Definition |
 	Return |
-	Attribute |
 	Each |
 	For |
 	Loop |
@@ -74,198 +72,200 @@ type CoreProp = {
 };
 
 export type Namespace = CoreProp & {
-	type: 'ns';
-	name: string;
-	members: NamespaceMember[];
+	type: 'ns'; // 名前空間
+	name: string; // 空間名
+	members: NamespaceMember[]; // メンバー
 };
 
 export type Meta = CoreProp & {
-	type: 'meta';
-	name: string | null;
-	value: StaticLiteral;
+	type: 'meta'; // メタデータ定義
+	name: string | null; // 名
+	value: StaticLiteral; // 値
 };
 
 export type Definition = CoreProp & {
-	type: 'def';
-	name: string;
-	varType?: TypeSource;
-	expr: Expression;
-	mut: boolean;
-	attr: Attribute[];
-};
-
-export type Return = CoreProp & {
-	type: 'return';
-	expr: Expression;
+	type: 'def'; // 変数宣言文
+	name: string; // 変数名
+	varType?: TypeSource; // 変数の型
+	expr: Expression; // 式
+	mut: boolean; // ミュータブルか否か
+	attr: Attribute[]; // 付加された属性
 };
 
 export type Attribute = CoreProp & {
-	type: 'attr';
-	name: string;
-	value: StaticLiteral;
+	type: 'attr'; // 属性
+	name: string; // 属性名
+	value: StaticLiteral; // 値
+};
+
+export type Return = CoreProp & {
+	type: 'return'; // return文
+	expr: Expression; // 式
 };
 
 export type Each = CoreProp & {
-	type: 'forOf';
-	var: string;
-	items: Expression;
-	for: LocalMember;
+	type: 'forOf'; // each文
+	var: string; // イテレータ変数名
+	items: Expression; // 配列
+	for: Statement | Expression; // 本体処理
 };
 
 export type For = CoreProp & {
-	type: 'for';
-	var?: string;
-	from?: Expression;
-	to?: Expression;
-	times?: Expression;
-	for: LocalMember;
+	type: 'for'; // for文
+	var?: string; // イテレータ変数名
+	from?: Expression; // 開始値
+	to?: Expression; // 終値
+	times?: Expression; // 回数
+	for: Statement | Expression; // 本体処理
 };
 
 export type Loop = CoreProp & {
-	type: 'loop';
-	statements: LocalMember[];
+	type: 'loop'; // loop文
+	statements: (Statement | Expression)[]; // 処理
 };
 
 export type Break = CoreProp & {
-	type: 'break';
+	type: 'break'; // break文
 };
 
 export type Continue = CoreProp & {
-	type: 'continue';
+	type: 'continue'; // continue文
 };
 
 export type AddAssign = CoreProp & {
-	type: 'inc';
-	dest: Expression;
-	expr: Expression;
+	type: 'inc'; // 加算代入文
+	dest: Expression; // 代入先
+	expr: Expression; // 式
 };
 
 export type SubAssign = CoreProp & {
-	type: 'dec';
-	dest: Expression;
-	expr: Expression;
+	type: 'dec'; // 減算代入文
+	dest: Expression; // 代入先
+	expr: Expression; // 式
 };
 
 export type Assign = CoreProp & {
-	type: 'assign';
-	dest: Expression;
-	expr: Expression;
+	type: 'assign'; // 代入文
+	dest: Expression; // 代入先
+	expr: Expression; // 式
 };
 
 export type InfixOperator = "||" | "&&" | "==" | "!=" | "<=" | ">=" | "<" | ">" | "+" | "-" | "*" | "|" | "%";
 
 // TODO: remove by transform plugin
 export type Infix = CoreProp & {
-	type: 'infix';
-	operands: Expression[];
-	operators: InfixOperator[];
+	type: 'infix'; // 中置演算子式
+	operands: Expression[]; // 項のリスト
+	operators: InfixOperator[]; // 演算子のリスト
 };
 
 export type If = CoreProp & {
-	type: 'if';
-	cond: Expression;
-	then: LocalMember;
+	type: 'if'; // if式
+	cond: Expression; // 条件式
+	then: Statement | Expression; // then節
 	elseif: {
-		cond: Expression;
-		then: LocalMember;
+		cond: Expression; // elifの条件式
+		then: Statement | Expression;// elif節
 	}[];
-	else?: LocalMember;
+	else?: Statement | Expression; // else節
 };
 
 export type Fn = CoreProp & {
-	type: 'fn';
+	type: 'fn'; // 関数
 	args: {
-		name: string;
-		type?: TypeSource;
+		name: string; // 引数名
+		type?: TypeSource; // 引数の型
 	}[];
-	ret?: TypeSource; 
-	children: LocalMember[];
+	ret?: TypeSource; // 戻り値の型
+	children: (Statement | Expression)[]; // 本体処理
 };
 
 export type Match = CoreProp & {
-	type: 'match';
-	about: Expression;
+	type: 'match'; // パターンマッチ
+	about: Expression; // 対象
 	qs: {
-		q: Expression;
-		a: LocalMember;
+		q: Expression; // 条件
+		a: Statement | Expression; // 結果
 	}[];
-	default?: LocalMember;
+	default?: Statement | Expression; // デフォルト値
 };
 
 export type Eval = CoreProp & {
-	type: 'block';
-	statements: LocalMember[];
+	type: 'block'; // ブロックまたはeval式
+	statements: (Statement | Expression)[]; // 処理
 };
 
 export type Tmpl = CoreProp & {
-	type: 'tmpl';
-	tmpl: (string | Expression)[];
+	type: 'tmpl'; // テンプレート
+	tmpl: (string | Expression)[]; // 処理
 };
 
 export type Str = CoreProp & {
-	type: 'str';
-	value: string;
+	type: 'str'; // 文字列リテラル
+	value: string; // 文字列
 };
 
 export type Num = CoreProp & {
-	type: 'num';
-	value: number;
+	type: 'num'; // 数値リテラル
+	value: number; // 数値
 };
 
 export type Bool = CoreProp & {
-	type: 'bool';
-	value: boolean;
+	type: 'bool'; // 真理値リテラル
+	value: boolean; // 真理値
 };
 
 export type Null = CoreProp & {
-	type: 'null';
+	type: 'null'; // nullリテラル
 };
 
 export type Obj = CoreProp & {
-	type: 'obj';
-	value: Map<string, Expression>;
+	type: 'obj'; // オブジェクト
+	value: Map<string, Expression>; // プロパティ
 };
 
 export type Arr = CoreProp & {
-	type: 'arr';
-	value: Expression[];
+	type: 'arr'; // 配列
+	value: Expression[]; // アイテム
 };
 
 export type Var = CoreProp & {
-	type: 'var';
-	name: string;
+	type: 'var'; // 変数
+	name: string; // 変数名
 };
 
 export type StaticObj = CoreProp & {
-	type: 'obj';
-	value: Map<string, StaticLiteral>;
+	type: 'obj'; // 静的なオブジェクト
+	value: Map<string, StaticLiteral>; // プロパティ
 };
 
 export type StaticArr = CoreProp & {
-	type: 'arr';
-	value: StaticLiteral[];
+	type: 'arr'; // 静的な配列
+	value: StaticLiteral[]; // アイテム
 };
 
-// [ex.] call > var
-// [ex.] call > fn
+// chain node example:
+// call > fn
+// call > var(fn)
+// index > arr
+// index > var(arr)
+// prop > prop(obj) > var(obj)
+// call > prop(fn) > obj
+
 export type Call = CoreProp & {
-	type: 'call';
-	expr: ChainSource;
-	args: Expression[];
+	type: 'call'; // 関数呼び出し
+	expr: ChainSource; // 対象
+	args: Expression[]; // 引数
 };
 
-// [ex.] index > var
-// [ex.] index > arr
 export type Index = CoreProp & {
-	type: 'index';
-	expr: ChainSource;
-	index: Expression;
+	type: 'index'; // 配列要素アクセス
+	expr: ChainSource; // 対象
+	index: Expression; // インデックス
 };
 
-// [ex.] prop > var
-// [ex.] prop > obj
 export type Prop = CoreProp & {
-	type: 'prop';
-	expr: ChainSource;
-	name: string;
+	type: 'prop'; // プロパティアクセス
+	expr: ChainSource; // 対象
+	name: string; // プロパティ名
 };
