57ee2bc4e99df39b93c3c8dc39430439a4923677
marihachi <marihachi0620@gmail.com>
2022-08-21 20:41:31 +0900

    fix keyword validation

--- src/parser/plugins/validate-keyword.ts
+++ src/parser/plugins/validate-keyword.ts
@@ -1,5 +1,5 @@
 import * as aiscript from '../..';
-import { Node } from '../node';
+import { Node, isChainHost } from '../node';
 
 const reservedWord = [
 	'null',
@@ -44,49 +44,78 @@ function throwReservedWordError(name: string) {
 	throw new aiscript.SemanticError(`Reserved word "${name}" cannot be used as variable name.`);
 }
 
-export function validateKeyword(nodes: Node[]): Node[] {
-	for (const node of nodes) {
-		switch (node.type) {
-			case 'def':
-			case 'ns': {
-				if (reservedWord.includes(node.name)) {
-					throwReservedWordError(node.name);
-				}
-				break;
+function validate(node: Node): void {
+	switch (node.type) {
+		case 'def':
+		case 'ns': {
+			if (reservedWord.includes(node.name)) {
+				throwReservedWordError(node.name);
 			}
-			case 'assign': {
-				if (node.dest.type === 'var' && node.dest.chain == null) {
-					if (reservedWord.includes(node.dest.name)) {
-						throwReservedWordError(node.dest.name);
-					}
-				}
-				break;
-			}
-			case 'fn': {
-				validateKeyword(node.children);
-				break;
-			}
-			case 'block': {
-				validateKeyword(node.statements);
-				break;
-			}
-			case 'if': {
-				if (node.then.type === 'block') {
-					validateKeyword(node.then.statements);
-				}
-				for (const n of node.elseif) {
-					if (n.then.type === 'block') {
-						validateKeyword(n.then.statements);
-					}
-				}
-				if (node.else?.type === 'block') {
-					validateKeyword(node.else.statements);
-				}
-				break;
-			}
-			// TODO: match
+			break;
 		}
+		case 'inc':
+		case 'dec':
+		case 'assign': {
+			if (node.dest.type === 'var') {
+				if (reservedWord.includes(node.dest.name)) {
+					throwReservedWordError(node.dest.name);
+				}
+			}
+			validate(node.dest);
+			validate(node.expr);
+			break;
+		}
+		case 'fn': {
+			for (const inner of node.children) {
+				validate(inner);
+			}
+			break;
+		}
+		case 'block': {
+			for (const inner of node.statements) {
+				validate(inner);
+			}
+			break;
+		}
+		case 'if': {
+			if (node.then.type === 'block') {
+				for (const inner of node.then.statements) {
+					validate(inner);
+				}
+			}
+			for (const n of node.elseif) {
+				if (n.then.type === 'block') {
+					for (const inner of n.then.statements) {
+						validate(inner);
+					}
+				}
+			}
+			if (node.else?.type === 'block') {
+				for (const inner of node.else.statements) {
+					validate(inner);
+				}
+			}
+			break;
+		}
+		// TODO: match
 	}
 
+	if (isChainHost(node)) {
+		if (node.chain != null) {
+			for (const item of node.chain) {
+				if (item.type === 'propChain') {
+					if (reservedWord.includes(item.name)) {
+						throwReservedWordError(item.name);
+					}
+				}
+			}
+		}
+	}
+}
+
+export function validateKeyword(nodes: Node[]): Node[] {
+	for (const inner of nodes) {
+		validate(inner);
+	}
 	return nodes;
 }
