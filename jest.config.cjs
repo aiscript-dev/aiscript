929d91bd0eb084a6705a0dd1b48bf77083d1b768
FineArchs <133759614+FineArchs@users.noreply.github.com>
2023-05-18 20:33:16 +0900

    警告に従いjest.config.cjsを更新 (#272)
    * Update get-started.md
    
    ・標準関数（std関数？）およびプリミティブ関数について追記
    ・その他細かい変更
    
    * update jest.config.cjs
    
    ---------
    
    Co-authored-by: FineArchs <noneffectiveaddress@gmail.com>

--- jest.config.cjs
+++ jest.config.cjs
@@ -60,14 +60,14 @@ module.exports = {
 	// globalTeardown: undefined,
 
 	// A set of global variables that need to be available in all test environments
-	globals: {
-		"ts-jest": {
-			"useESM": true,
-			diagnostics: {
-				exclude: ['!test/**/*.ts'],
-			},
-		}
-	},
+	//globals: {
+		//"ts-jest": {
+			//"useESM": true,
+			//diagnostics: {
+				//exclude: ['!test/**/*.ts'],
+			//},
+		//}
+	//},
 
 	// The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
 	// maxWorkers: "50%",
@@ -181,7 +181,15 @@ module.exports = {
 
 	// A map from regular expressions to paths to transformers
 	transform: {
-		"^.+\\.(ts|tsx)$": "ts-jest"
+		"^.+\\.(ts|tsx)$": [
+			"ts-jest",
+			{
+				"useESM": true,
+				diagnostics: {
+					exclude: ['!test/**/*.ts'],
+				},
+			},
+		],
 	},
 
 	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
