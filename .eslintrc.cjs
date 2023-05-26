8ab9c279009fe8453c9a65cf3bb7b359d081bb56
tsukimizake <shomasd@gmail.com>
2023-05-26 19:56:41 +0900

    disable no-non-null-assertion (#275)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -64,7 +64,7 @@ module.exports = {
 		'@typescript-eslint/no-var-requires': ['warn'],
 		'@typescript-eslint/no-inferrable-types': ['warn'],
 		'@typescript-eslint/no-empty-function': ['off'],
-		'@typescript-eslint/no-non-null-assertion': ['warn'],
+		'@typescript-eslint/no-non-null-assertion': ['off'],
 		'@typescript-eslint/explicit-function-return-type': ['warn'],
 		'@typescript-eslint/no-misused-promises': ['error', {
 			'checksVoidReturn': false,
