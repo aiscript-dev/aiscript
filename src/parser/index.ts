359b4cecdf561a0476046ac9c3cf1015b169b0a6
FineArchs <133759614+FineArchs@users.noreply.github.com>
2023-08-27 07:14:25 +0900

    importに拡張子を追加

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,13 +1,13 @@
-import { SyntaxError } from '../error';
+import { SyntaxError } from '../error.js';
 import * as parser from './parser.js';
 
-import { validateKeyword } from './plugins/validate-keyword';
-import { validateType } from './plugins/validate-type';
-import { setAttribute } from './plugins/set-attribute';
-import { transformChain } from './plugins/transform-chain';
-import { infixToFnCall } from './plugins/infix-to-fncall';
-import type * as Cst from './node';
-import type * as Ast from '../node';
+import { validateKeyword } from './plugins/validate-keyword.js';
+import { validateType } from './plugins/validate-type.js';
+import { setAttribute } from './plugins/set-attribute.js';
+import { transformChain } from './plugins/transform-chain.js';
+import { infixToFnCall } from './plugins/infix-to-fncall.js';
+import type * as Cst from './node.js';
+import type * as Ast from '../node.js';
 
 export type ParserPlugin = (nodes: Cst.Node[]) => Cst.Node[];
 export type PluginType = 'validate' | 'transform';
