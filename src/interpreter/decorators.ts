b40cc440da2f854739839865bfbbb95128407341
FineArchs <133759614+FineArchs@users.noreply.github.com>
2023-08-27 07:23:04 +0900

    Create decorators.ts
    autobind-decoratorがそのままesmで使えないことへの対策

---
+++ src/interpreter/decorators.ts
@@ -0,0 +1,48 @@
+// copied from https://github.com/misskey-dev/misskey/blob/develop/packages/backend/src/decorators.ts
+
+/*
+ * SPDX-FileCopyrightText: syuilo and other misskey contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+// https://github.com/andreypopp/autobind-decorator
+
+/**
+ * Return a descriptor removing the value and returning a getter
+ * The getter will return a .bind version of the function
+ * and memoize the result against a symbol on the instance
+ */
+export function bindThis(target: any, key: string, descriptor: any) {
+	let fn = descriptor.value;
+
+	if (typeof fn !== 'function') {
+		throw new TypeError(`@bindThis decorator can only be applied to methods not: ${typeof fn}`);
+	}
+
+	return {
+		configurable: true,
+		get() {
+			// eslint-disable-next-line no-prototype-builtins
+			if (this === target.prototype || this.hasOwnProperty(key) ||
+        typeof fn !== 'function') {
+				return fn;
+			}
+
+			const boundFn = fn.bind(this);
+			Object.defineProperty(this, key, {
+				configurable: true,
+				get() {
+					return boundFn;
+				},
+				set(value) {
+					fn = value;
+					delete this[key];
+				},
+			});
+			return boundFn;
+		},
+		set(value: any) {
+			fn = value;
+		},
+	};
+}
