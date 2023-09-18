31f18096b37f082a7d25c37e2aa9e8a26afd51a0
marihachi <marihachi0620@gmail.com>
2023-09-18 19:28:33 +0900

    debug

--- parse.js
+++ parse.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
-import { Parser } from '@syuilo/aiscript';
+import { Parser } from './built/esm/parser/index.js';
 
-const script = fs.readFileSync('./test.is', 'utf8');
+const script = fs.readFileSync('./debug.ais', 'utf8');
 const ast = Parser.parse(script);
 console.log(JSON.stringify(ast, null, 2));
