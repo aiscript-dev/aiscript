c70ed01dc6baf0012b89b837ce7f0575ec965f5a
marihachi <marihachi0620@gmail.com>
2023-09-18 12:15:39 +0900

    add test

---
+++ test/parser.ts
@@ -0,0 +1,47 @@
+import * as assert from 'assert';
+import { TokenStream } from '../src/parser/token-stream';
+import { TOKEN, TokenKind } from '../src/parser/token';
+
+describe('TokenStream', () => {
+	function next(stream: TokenStream, kind: TokenKind, value?: string) {
+		stream.read();
+		assert.deepStrictEqual(stream.current, TOKEN(kind, value));
+	}
+
+	test.concurrent('eof', async () => {
+		const source = '';
+		const stream = new TokenStream(source);
+		next(stream, TokenKind.EOF);
+		next(stream, TokenKind.EOF);
+	});
+	test.concurrent('keyword', async () => {
+		const source = 'if';
+		const stream = new TokenStream(source);
+		next(stream, TokenKind.IfKeyword);
+		next(stream, TokenKind.EOF);
+	});
+	test.concurrent('identifier', async () => {
+		const source = 'xyz';
+		const stream = new TokenStream(source);
+		next(stream, TokenKind.Identifier, 'xyz');
+		next(stream, TokenKind.EOF);
+	});
+	test.concurrent('words', async () => {
+		const source = 'abc xyz';
+		const stream = new TokenStream(source);
+		next(stream, TokenKind.Identifier, 'abc');
+		next(stream, TokenKind.Identifier, 'xyz');
+		next(stream, TokenKind.EOF);
+	});
+	test.concurrent('stream', async () => {
+		const source = '@abc() { }';
+		const stream = new TokenStream(source);
+		next(stream, TokenKind.At);
+		next(stream, TokenKind.Identifier, 'abc');
+		next(stream, TokenKind.OpenParen);
+		next(stream, TokenKind.CloseParen);
+		next(stream, TokenKind.OpenBrace);
+		next(stream, TokenKind.CloseBrace);
+		next(stream, TokenKind.EOF);
+	});
+});
