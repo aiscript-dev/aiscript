9216297dd9a5aea8d82787ccf6cb6298509ff48d
Fine Archs <cicada1003@gmail.com>
2023-09-23 10:34:44 +0900

    add test

--- test/index.ts
+++ test/index.ts
@@ -2627,6 +2627,13 @@ describe('std', () => {
 				<: Core:to_str(arr)
 			`), STR('[ { value: ... } ]'));
 		});
+
+		test.concurrent('abort', async () => {
+			assert.rejects(
+				exe('Core:abort("hoge")'),
+				{ name: '', message: 'hoge' },
+			);
+		});
 	});
 
 	describe('Arr', () => {
