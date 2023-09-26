0cf0bec73f6337705081bfca1c1b3cc197b42ecc
marihachi <marihachi0620@gmail.com>
2023-09-26 21:36:00 +0900

    enhance CharStream

--- src/parser/streams/char-stream.ts
+++ src/parser/streams/char-stream.ts
@@ -2,21 +2,28 @@
  * 入力文字列から文字を読み取るクラス
 */
 export class CharStream {
-	private source: string;
-	private index: number;
+	private pages: Map<number, string>;
+	private firstPageIndex: number;
+	private lastPageIndex: number;
+	private pageIndex: number;
+	private address: number;
 	private _char?: string;
 
 	constructor(source: string) {
-		this.source = source;
-		this.index = 0;
+		this.pages = new Map();
+		this.pages.set(0, source);
+		this.firstPageIndex = 0;
+		this.lastPageIndex = 0;
+		this.pageIndex = 0;
+		this.address = 0;
 	}
 
 	public init(): void {
-		this.load();
+		this.loadChar();
 	}
 
 	public get eof(): boolean {
-		return (this.index >= this.source.length);
+		return this.endOfPage && this.isLastPage;
 	}
 
 	public get char(): string {
@@ -31,15 +38,41 @@ export class CharStream {
 	}
 
 	public next(): void {
-		if (!this.eof) {
-			this.index++;
+		if (!this.endOfPage) {
+			this.address++;
+		} else if (!this.isLastPage) {
+			this.pageIndex++;
+			this.address = 0;
 		}
-		this.load();
+		this.loadChar();
 	}
 
-	private load(): void {
+	public prev(): void {
+		if (this.address > 0) {
+			this.address--;
+		} else if (!this.isFirstPage) {
+			this.pageIndex--;
+			this.address = this.pages.get(this.pageIndex)!.length - 1;
+		}
+		this.loadChar();
+	}
+
+	private get isFirstPage() {
+		return (this.pageIndex <= this.firstPageIndex);
+	}
+
+	private get isLastPage() {
+		return (this.pageIndex >= this.lastPageIndex);
+	}
+
+	private get endOfPage(): boolean {
+		const page = this.pages.get(this.pageIndex)!;
+		return (this.address >= page.length);
+	}
+
+	private loadChar(): void {
 		if (!this.eof) {
-			this._char = this.source[this.index];
+			this._char = this.pages.get(this.pageIndex)![this.address];
 		}
 	}
 }
--- test/parser.ts
+++ test/parser.ts
@@ -1,6 +1,57 @@
 import * as assert from 'assert';
 import { Scanner } from '../src/parser/scanner';
 import { TOKEN, TokenKind } from '../src/parser/token';
+import { CharStream } from '../src/parser/streams/char-stream';
+
+describe('CharStream', () => {
+	test.concurrent('char', async () => {
+		const source = 'abc';
+		const stream = new CharStream(source);
+		stream.init();
+		assert.strictEqual('a', stream.char);
+	});
+
+	test.concurrent('next', async () => {
+		const source = 'abc';
+		const stream = new CharStream(source);
+		stream.init();
+		stream.next();
+		assert.strictEqual('b', stream.char);
+	});
+
+	describe('prev', () => {
+		test.concurrent('move', async () => {
+			const source = 'abc';
+			const stream = new CharStream(source);
+			stream.init();
+			stream.next();
+			assert.strictEqual('b', stream.char);
+			stream.prev();
+			assert.strictEqual('a', stream.char);
+		});
+
+		test.concurrent('境界外には移動しない', async () => {
+			const source = 'abc';
+			const stream = new CharStream(source);
+			stream.init();
+			stream.prev();
+			assert.strictEqual('a', stream.char);
+		});
+	});
+
+	test.concurrent('eof', async () => {
+		const source = 'abc';
+		const stream = new CharStream(source);
+		stream.init();
+		assert.strictEqual(false, stream.eof);
+		stream.next();
+		assert.strictEqual(false, stream.eof);
+		stream.next();
+		assert.strictEqual(false, stream.eof);
+		stream.next();
+		assert.strictEqual(true, stream.eof);
+	});
+});
 
 describe('Scanner', () => {
 	function init(source: string) {
