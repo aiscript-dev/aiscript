0c7105ee3ccaee54fcb49d8c313ebe3130bc7cb7
marihachi <marihachi0620@gmail.com>
2023-11-07 22:04:14 +0900

    refactor

--- src/interpreter/deep-equal.ts
+++ src/interpreter/deep-equal.ts
@@ -2,32 +2,34 @@ export function deepEqual(a: unknown, b: unknown): boolean {
 	return deepEqualRefs(a, b, [], []);
 }
 
-function deepEqualRefs(a: unknown, b: unknown, prevRefsA: unknown[], prevRefsB: unknown[]): boolean {
+function deepEqualRefs(a: unknown, b: unknown, refsA: unknown[], refsB: unknown[]): boolean {
+	// NOTE: Object.is()ではNaN同士の比較結果もtrue
 	if (Object.is(a, b)) return true;
 
-	// object
+	// Object
 	if (a !== null && b !== null && typeof a === 'object' && typeof b === 'object') {
-		const refsA = [...prevRefsA, a];
-		const refsB = [...prevRefsB, b];
-
-		// 循環チェック
-		const indexA = prevRefsA.findIndex(x => x === a);
-		const indexB = prevRefsB.findIndex(x => x === b);
+		// 参照の循環をチェック
 		// NOTE: indexA,Bの一致を確認しているが、これで良いのか怪しい
+		const indexA = refsA.findIndex(x => x === a);
+		const indexB = refsB.findIndex(x => x === b);
 		if (indexA !== -1 && indexB !== -1 && indexA === indexB) {
 			return true;
 		}
 
-		// array
+		// 次の参照パスを生成
+		const nextRefsA = [...refsA, a];
+		const nextRefsB = [...refsB, b];
+
+		// Array
 		if (Array.isArray(a) && Array.isArray(b)) {
 			if (a.length !== b.length) return false;
 			for (let i = 0; i < a.length; i++) {
-				if (!deepEqualRefs(a[i], b[i], refsA, refsB)) return false;
+				if (!deepEqualRefs(a[i], b[i], nextRefsA, nextRefsB)) return false;
 			}
 			return true;
 		}
 
-		// map
+		// Map
 		if (a instanceof Map && b instanceof Map) {
 			if (a.size !== b.size) return false;
 			const aEntries = a.entries();
@@ -35,13 +37,13 @@ function deepEqualRefs(a: unknown, b: unknown, prevRefsA: unknown[], prevRefsB: 
 			for (let i = 0; i < a.size; i++) {
 				const entryA = aEntries.next();
 				const entryB = bEntries.next();
-				if (!deepEqualRefs(entryA.value[0], entryB.value[0], refsA, refsB)) return false;
-				if (!deepEqualRefs(entryA.value[1], entryB.value[1], refsA, refsB)) return false;
+				if (!deepEqualRefs(entryA.value[0], entryB.value[0], nextRefsA, nextRefsB)) return false;
+				if (!deepEqualRefs(entryA.value[1], entryB.value[1], nextRefsA, nextRefsB)) return false;
 			}
 			return true;
 		}
 
-		// set
+		// Set
 		if (a instanceof Set && b instanceof Set) {
 			if (a.size !== b.size) return false;
 			const aValues = a.values();
@@ -49,7 +51,7 @@ function deepEqualRefs(a: unknown, b: unknown, prevRefsA: unknown[], prevRefsB: 
 			for (let i = 0; i < a.size; i++) {
 				const valueA = aValues.next();
 				const valueB = bValues.next();
-				if (!deepEqualRefs(valueA.value, valueB.value, refsA, refsB)) return false;
+				if (!deepEqualRefs(valueA.value, valueB.value, nextRefsA, nextRefsB)) return false;
 			}
 			return true;
 		}
@@ -57,7 +59,7 @@ function deepEqualRefs(a: unknown, b: unknown, prevRefsA: unknown[], prevRefsB: 
 		// object keys
 		const keys = Object.keys(a);
 		for (const key of keys) {
-			if (!deepEqualRefs((a as Record<string, unknown>)[key], (b as Record<string, unknown>)[key], refsA, refsB)) return false;
+			if (!deepEqualRefs((a as Record<string, unknown>)[key], (b as Record<string, unknown>)[key], nextRefsA, nextRefsB)) return false;
 		}
 		return true;
 	}
