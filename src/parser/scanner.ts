97b9d15e5ad81a5e89241b96e96159f0082b99ec
Take-John <takejohn@takejohn.jp>
2025-01-03 17:39:49 +0900

    fix: 複数行コメントの*/がないと文法エラーが出るように (#887)
    * Fix: 複数行コメントの*/がないと文法エラーが出るように
    
    * CHANGELOG削除

--- src/parser/scanner.ts
+++ src/parser/scanner.ts
@@ -628,10 +628,13 @@ export class Scanner implements ITokenStream {
 	private skipCommentRange(): void {
 		while (true) {
 			if (this.stream.eof) {
-				break;
+				throw new AiScriptUnexpectedEOFError(this.stream.getPos());
 			}
 			if (this.stream.char === '*') {
 				this.stream.next();
+				if (this.stream.eof) {
+					throw new AiScriptUnexpectedEOFError(this.stream.getPos());
+				}
 				if ((this.stream.char as string) === '/') {
 					this.stream.next();
 					break;
