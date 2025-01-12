bae8bdb93e07a0f380f0add99e2918701f2a350a
FineArchs <133759614+FineArchs@users.noreply.github.com>
2025-01-12 14:22:15 +0900

    add Aison.parse type
    Co-authored-by: uzmoi <rizzzse@gmail.com>

--- src/parser/aison.ts
+++ src/parser/aison.ts
@@ -1,17 +1,16 @@
 /**
  * AiSON: AiScript Object Notation
  */
+import type { JsValue } from '../interpreter/util.js';
 import { nodeToJs } from '../utils/node-to-js.js';
 import { Scanner } from './scanner.js';
 import { parseAiSonTopLevel } from './syntaxes/aison.js';
 
 export class AiSON {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public static parse(input: string): any {
+	public static parse(input: string): JsValue {
 		const scanner = new Scanner(input);
 		const ast = parseAiSonTopLevel(scanner);
 
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		return nodeToJs(ast) as any;
+		return nodeToJs(ast);
 	}
 }
