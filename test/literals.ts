77cf3fd3a187021d02bbc0e46c546720480b8d41
Take-John <takejohn@takejohn.jp>
2025-09-10 13:39:08 +0900

    enhance: 数値リテラルに指数表記を追加 (#954)
    * 数値リテラルに指数表記を追加
    
    * next()メソッドの呼び出し位置が気になるので変更

--- test/literals.ts
+++ test/literals.ts
@@ -58,6 +58,33 @@ describe('literal', () => {
 		eq(res, NUM(0.5));
 	});
 
+	test.concurrent('number (positive exponent without plus sign)', async () => {
+		const res = await exe(`
+		<: 1.2e3
+		`);
+		eq(res, NUM(1200));
+	});
+
+	test.concurrent('number (positive exponent with plus sign)', async () => {
+		const res = await exe(`
+		<: 1.2e+3
+		`);
+		eq(res, NUM(1200));
+	});
+
+	test.concurrent('number (negative exponent)', async () => {
+		const res = await exe(`
+		<: 1.2e-3
+		`);
+		eq(res, NUM(0.0012));
+	});
+
+	test.concurrent('number (missing exponent)', async () => {
+		assert.rejects(() => exe(`
+		<: 1.2e+
+		`), 'exponent expected');
+	});
+
 	test.concurrent('arr (separated by comma)', async () => {
 		const res = await exe(`
 		<: [1, 2, 3]
